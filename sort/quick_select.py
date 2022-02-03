b9741112e19feead74766d83c66c339d1a6bc14a
jmsmg <jmsmg1@me.com>
2022-02-03 03:11:26 +0000

    반복 부분 고려 요망

---
+++ sort/quick_select.py
@@ -0,0 +1,15 @@
+def quick_select(array:list, rank:int) -> int:
+    if len(array) < 2:
+        return -1
+    
+    idx_l = 0
+    idx_r = len(array) - 2
+    mid = idx_r // 2
+    pivot = len(array) - 1
+    
+    while 
+    
+array = list(map(int, input().split()))
+rank = int(input())
+
+print(quick_select(array, rank))
