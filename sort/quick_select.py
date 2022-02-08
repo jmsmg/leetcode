65a3e4e7b00c121d05d22f57e9138cbfcae8cd2f
jmsmg <jmsmg1@me.com>
2022-02-08 09:43:43 +0000

    재귀 리턴에서 막힘

--- sort/quick_select.py
+++ sort/quick_select.py
@@ -1,14 +1,27 @@
 def quick_select(array:list, rank:int) -> int:
     if len(array) < 2:
         return -1
-    
-    idx_l = 0
-    idx_r = len(array) - 2
-    mid = idx_r // 2
-    pivot = len(array) - 1
-    
-    while 
-    
+
+    L = 0
+    R = len(array) - 2
+    array[len(array)-3], array[len(array)-1] = array[len(array)-1], array[len(array)-3]
+    pivot = len(array)-1 
+
+    while L < R:
+        if array[L] < array[pivot]:
+            L =+ 1
+        if array[pivot] < array[R]:
+            R =- 1
+        if array[L] > array[pivot] and array[R] < array[pivot]:
+            array[L], array[R] = array[R], array[L]
+
+    array[L], array[pivot] = array[pivot], array[L]
+
+    if L < rank:
+        return quick_select(array[R:], rank) +
+    elif R > rank:
+        return quick_select(array[:L], rank)
+
 array = list(map(int, input().split()))
 rank = int(input())
 
